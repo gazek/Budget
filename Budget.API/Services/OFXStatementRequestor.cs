709b45aeaadcd67ff2de65a7e0226caa0cd5d2b7
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-10-04 16:53:21 -0700

    Add file to project

---
+++ Budget.API/Services/OFXStatementRequestor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+namespace Budget.API.Services
+{
+    public class OFXStatementRequestor
+    {
+        string RequestStatus
+        {
+            get { return _response; }
+        }
+        OFXStatementRequestConfig _config;
+        OFXStatementRequestBuilder _requestBuilder;
+        private string _response;
+
+        public OFXStatementRequestor(OFXStatementRequestConfig config)
+        {
+            _config = config;
+            _requestBuilder = new OFXStatementRequestBuilder(_config);
+        }
+
+        void PostToFinancialInsitution()
+        {
+            using (var client = new WebClient())
+            {
+                client.BaseAddress = _config.URL.AbsoluteUri;
+                var address = new Uri("");
+                _response = client.UploadString(address, _requestBuilder.Request);
+            }
+        }
+    }
+}
