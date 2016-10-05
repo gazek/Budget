49fb0f1247f72303cdab3f90ea163a9c0c1f77e3
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-10-05 16:49:14 -0700

    Added POST response tests

--- Budget.Tests/Services/OFXClient/OFXRequestorTest.cs
+++ Budget.Tests/Services/OFXClient/OFXRequestorTest.cs
@@ -27,13 +27,23 @@ namespace Budget.Tests.Services
                 IncludeTransactions = true
             };
             OFXRequestor request = new OFXRequestor(config);
+            string expectedHeader = "OFXHEADER:100 DATA:OFXSGML VERSION:102 SECURITY:NONE ENCODING:USASCII CHARSET:1252 COMPRESSION:NONE OLDFILEUID:NONE NEWFILEUID:NONE\r\nDATA:OFXSGML\r\nVERSION:102\r\nSECURITY:NONE\r\nENCODING:USASCII\r\nCHARSET:1252\r\nCOMPRESSION:NONE\r\nOLDFILEUID:NONE\r\nNEWFILEUID:NONE\r\n\r\n";
+            string expectedOfx = "<OFX><SIGNONMSGSRSV1><SONRS><STATUS><CODE>15500<SEVERITY>ERROR<MESSAGE>User or Member password invalid</STATUS><DTSERVER>20161005152847.025[-7:PDT]<LANGUAGE>ENG<FI><ORG>First Tech Federal Credit Union<FID>3169</FI></SONRS></SIGNONMSGSRSV1><BANKMSGSRSV1><STMTTRNRS><TRNUID>1001<STATUS><CODE>15500<SEVERITY>ERROR</STATUS></STMTTRNRS></BANKMSGSRSV1></OFX>";
+            string expected = expectedHeader + expectedOfx;
+            int indexOfxL = expectedOfx.IndexOf("<DTSERVER>");
+            int indexOfxR = expectedOfx.IndexOf("<LANGUAGE>");
+            int indexL = expected.IndexOf("<DTSERVER>");
+            int indexR = expected.IndexOf("<LANGUAGE>");
 
             // Act
             request.Post();
 
             // Assert
-            Assert.IsNotNull(request.Response);
-
+            Assert.AreEqual(request.Header.Replace("\r\n ", string.Empty), expectedHeader.Replace("\r\n ", string.Empty));
+            Assert.AreEqual(request.OFX.Substring(0, indexOfxL), expectedOfx.Substring(0, indexOfxL));
+            Assert.AreEqual(request.OFX.Substring(indexOfxR), expectedOfx.Substring(indexOfxR));
+            Assert.AreEqual(request.Response.Substring(0, indexL), expected.Substring(0, indexL));
+            Assert.AreEqual(request.Response.Substring(indexR), expected.Substring(indexR));
         }
     }
 }
