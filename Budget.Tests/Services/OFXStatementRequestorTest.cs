c18954f28c0032df9e62f77bafb0ecaa1b87f4c1
Greg Waliczek <gregory.waliczek@gmail.com>
2016-10-04 22:36:29 -0700

    Added file to project

---
+++ Budget.Tests/Services/OFXStatementRequestorTest.cs
@@ -0,0 +1,42 @@
+﻿using Budget.API.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.Tests.Services
+{
+    [TestClass]
+    public class OFXStatementRequestorTest
+    {
+        [TestMethod]
+        public void OFXStatementRequestorPostTest()
+        {
+            // Arrange
+            OFXStatementRequestConfig config = new OFXStatementRequestConfig
+            {
+                UserId = "fakeuser",
+                password = "fakepass!",
+                InstitutionName = "First Tech Federal Credit Union",
+                InstitutionId = 3169,
+                InstitutionRoutingNumber = 321180379,
+                AccountNumber = "1234567890",
+                AccountType = OFXRequestBuilderConfigAccountType.SAVINGS,
+                StartDate = new DateTime(2016, 9, 1),
+                EndDate = new DateTime(2016, 9, 30),
+                URL = new Uri("https://ofx.firsttechfed.com"),
+                IncludeTransactions = true
+            };
+            OFXStatementRequestor request = new OFXStatementRequestor(config);
+
+            // Act
+            request.Post();
+
+            // Assert
+            Assert.IsNotNull(request.Response);
+
+        }
+    }
+}
