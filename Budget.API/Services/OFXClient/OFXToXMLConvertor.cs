e10fbd978904d4fd5bf46fe6be0b819b563059e8
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-10-05 16:49:45 -0700

    Add file to project

---
+++ Budget.API/Services/OFXClient/OFXToXMLConvertor.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+namespace Budget.API.Services.OFXClient
+{
+    public class OFXToXMLConvertor
+    {
+        // Converting from (pre-version 2.0) OFX to XML is just a matter of
+        // adding closing tags to atom elements
+
+        public string Ofx
+        {
+            get { return _ofx; }
+            set { _ofx = value.Replace("\r\n", string.Empty); }
+        }
+        public string Xml
+        {
+            get { return _xml; }
+        }
+        string _xml;
+        string _ofx;
+
+        public void Convert()
+        {
+            // build new xml string
+            StringBuilder xml = new StringBuilder();
+
+            // initialize to first char of input OFX string
+            xml.Append(_ofx[0]);
+
+            // last tag scanned
+            string tag = "";
+
+            // track when the index is inside a tag
+            bool inTag = _ofx[0] == '<' ? true : false;
+
+            // track previous char
+            char prevChar = _ofx[0];
+
+            // walk across the string and 
+            // and place a closing tag whenever a '<' is found
+            // that wasn't preceded by a '>'
+            for (int i = 1; i < _ofx.Length; ++i)
+            {
+                // Clear previous tag name when new tag openning found
+                if (prevChar == '<')
+                {
+                    tag = "";
+                    inTag = true;
+                }
+
+                // Add closing tag
+                if (_ofx[i] == '<' && prevChar != '>')
+                {
+                    xml.Append("</"+tag+">");
+                }
+
+                // build XML string
+                xml.Append(_ofx[i]);
+
+                // build up tag name
+                if (inTag)
+                {
+                    if (_ofx[i] == '>')
+                    {
+                        inTag = false;
+                    }
+                    else
+                    {
+                        tag += _ofx[i].ToString();
+                    }
+                }
+
+                // set prev char
+                prevChar = _ofx[i];
+            }
+            _xml = xml.ToString();
+        }
+    }
+}
---
+++ Budget.Tests/Services/OFXClient/OFXToXMLConvertorTest.cs
@@ -0,0 +1,24 @@
+﻿using Budget.API.Services.OFXClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Budget.Tests.Services.OFXClient
+{
+    [TestClass]
+    public class OFXToXMLConvertorTest
+    {
+        [TestMethod]
+        public void OFXCloseTagsTest()
+        {
+            // Arrange
+            OFXToXMLConvertor convertor = new OFXToXMLConvertor();
+            string expectedXml = "<OFX><SIGNONMSGSRSV1><SONRS><STATUS><CODE>15500</CODE><SEVERITY>ERROR</SEVERITY><MESSAGE>User or Member password invalid</MESSAGE></STATUS><DTSERVER>20161005123252.423[-7:PDT]</DTSERVER><LANGUAGE>ENG</LANGUAGE><FI><ORG>First Tech Federal Credit Union</ORG><FID>3169</FID></FI></SONRS></SIGNONMSGSRSV1></OFX>";
+            convertor.Ofx = "<OFX><SIGNONMSGSRSV1><SONRS><STATUS><CODE>15500<SEVERITY>ERROR<MESSAGE>User or Member password invalid</STATUS><DTSERVER>20161005123252.423[-7:PDT]<LANGUAGE>ENG<FI><ORG>First Tech Federal Credit Union<FID>3169</FI></SONRS></SIGNONMSGSRSV1></OFX>";
+
+            // Act
+            convertor.Convert();
+
+            // Assert
+            Assert.AreEqual(convertor.Xml, expectedXml);
+        }
+    }
+}
