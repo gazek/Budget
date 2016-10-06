1a5fd52c82b788dd4a299bc9f47c7fa34c66ba0d
Greg Waliczek <gregory.waliczek@gmail.com>
2016-10-05 19:56:45 -0700

    Added tests to cover all request types

--- Budget.Tests/Services/OFXClient/OFXRequestBuilderTest.cs
+++ Budget.Tests/Services/OFXClient/OFXRequestBuilderTest.cs
@@ -61,11 +61,11 @@ namespace Budget.Tests.Controllers
         }
 
         [TestMethod]
-        public void OFXRequestBuilderBodyBankTest()
+        public void OFXRequestBuildeBankStatementTest()
         {
             // Arrange
             var config = CreateValidRequestBuilderConfig();
-            string expectedBody = GetValidRequestBodyString(OFXRequestConfigAccountType.CHECKING, true);
+            string expectedBody = GetValidStatementRequestBodyString(OFXRequestConfigAccountType.CHECKING);
 
             // Act
             OFXStatementRequestBuilder ofxBuilder = new OFXStatementRequestBuilder(config);
@@ -75,12 +75,12 @@ namespace Budget.Tests.Controllers
         }
 
         [TestMethod]
-        public void OFXRequestBuilderBodyCreditCardTest()
+        public void OFXRequestBuilderCreditCardStatementTest()
         {
             // Arrange
             var config = CreateValidRequestBuilderConfig();
             config.AccountType = OFXRequestConfigAccountType.CREDITCARD;
-            string expectedBody = GetValidRequestBodyString(OFXRequestConfigAccountType.CREDITCARD, true);
+            string expectedBody = GetValidStatementRequestBodyString(OFXRequestConfigAccountType.CREDITCARD);
 
             // Act
             OFXStatementRequestBuilder ofxBuilder = new OFXStatementRequestBuilder(config);
@@ -90,12 +90,42 @@ namespace Budget.Tests.Controllers
         }
 
         [TestMethod]
-        public void OFXRequestBuilderBodyIncludeTransTest()
+        public void OFXRequestBuilderBalanceTest()
         {
             // Arrange
             var config = CreateValidRequestBuilderConfig();
-            config.IncludeTransactions = false;
-            string expectedBody = GetValidRequestBodyString(OFXRequestConfigAccountType.CHECKING, false);
+            config.RequestType = OFXRequestConfigRequestType.Balance;
+            string expectedBody = GetValidBalanceRequestBodyString(OFXRequestConfigAccountType.CHECKING);
+
+            // Act
+            OFXStatementRequestBuilder ofxBuilder = new OFXStatementRequestBuilder(config);
+
+            // Assert
+            Assert.AreEqual(ofxBuilder.Body, expectedBody);
+        }
+
+        [TestMethod]
+        public void OFXRequestBuilderAccountListTest()
+        {
+            // Arrange
+            var config = CreateValidRequestBuilderConfig();
+            config.RequestType = OFXRequestConfigRequestType.AccountList;
+            string expectedBody = GetValidAccountListRequestBodyString();
+
+            // Act
+            OFXStatementRequestBuilder ofxBuilder = new OFXStatementRequestBuilder(config);
+
+            // Assert
+            Assert.AreEqual(ofxBuilder.Body, expectedBody);
+        }
+
+        [TestMethod]
+        public void OFXRequestBuilderSignOnTest()
+        {
+            // Arrange
+            var config = CreateValidRequestBuilderConfig();
+            config.RequestType = OFXRequestConfigRequestType.SignOn;
+            string expectedBody = GetValidSignOnRequestBodyString();
 
             // Act
             OFXStatementRequestBuilder ofxBuilder = new OFXStatementRequestBuilder(config);
@@ -108,6 +138,7 @@ namespace Budget.Tests.Controllers
         {
             var config = new OFXRequestConfig()
             {
+                RequestType = OFXRequestConfigRequestType.Statement,
                 UserId = "testUser",
                 password = "testPassword",
                 InstitutionName = "Some Bank",
@@ -117,13 +148,52 @@ namespace Budget.Tests.Controllers
                 AccountType = OFXRequestConfigAccountType.CHECKING,
                 StartDate = new DateTime(2016, 9, 1),
                 EndDate = new DateTime(2016, 9, 30),
-                URL = new Uri("https://fake.com"),
-                //IncludeTransactions = true
+                URL = new Uri("https://fake.com")
             };
             return config;
         }
 
-        private string GetValidRequestBodyString(OFXRequestConfigAccountType type, bool includeTrans)
+        private string GetValidSignOnRequestString()
+        {
+            string[] body = {
+                "<SIGNONMSGSRQV1>",
+                "<SONRQ>",
+                "<DTCLIENT>"+DateTime.Today.ToString("yyyyMMdd"),
+                "<USERID>testUser",
+                "<USERPASS>testPassword",
+                "<LANGUAGE>ENG",
+                "<FI>",
+                "<ORG>Some Bank",
+                "<FID>123",
+                "</FI>",
+                "<APPID>QWIN",
+                "<APPVER>1900",
+                "</SONRQ>",
+                "</SIGNONMSGSRQV1>"
+            };
+
+            string result = string.Join("", body);
+            return result;
+        }
+
+        private string GetValidAccountListRequestString()
+        {
+            string[] body = {
+                "<SIGNUPMSGSRQV1>",
+                "<ACCTINFOTRNRQ>",
+                "<TRNUID>1001",
+                "<ACCTINFORQ>",
+                "<DTACCTUP>20000101",
+                "</ACCTINFORQ>",
+                 "</ACCTINFOTRNRQ>",
+                "</SIGNUPMSGSRQV1>"
+            };
+
+            string result = string.Join("", body);
+            return result;
+        }
+
+        private string GetValidBankRequestString(OFXRequestConfigAccountType type, bool includeTrans)
         {
             string BANKMSGSRQV1 = "<BANKMSGSRQV1>";
             string STMTTRNRQ = "<STMTTRNRQ>";
@@ -157,21 +227,6 @@ namespace Budget.Tests.Controllers
             }
 
             string[] body = {
-                "<OFX>",
-                "<SIGNONMSGSRQV1>",
-                "<SONRQ>",
-                "<DTCLIENT>"+DateTime.Today.ToString("yyyyMMdd"),
-                "<USERID>testUser",
-                "<USERPASS>testPassword",
-                "<LANGUAGE>ENG",
-                "<FI>",
-                "<ORG>Some Bank",
-                "<FID>123",
-                "</FI>",
-                "<APPID>QWIN",
-                "<APPVER>1900",
-                "</SONRQ>",
-                "</SIGNONMSGSRQV1>",
                 BANKMSGSRQV1,
                 STMTTRNRQ,
                 "<TRNUID>1001",
@@ -188,11 +243,38 @@ namespace Budget.Tests.Controllers
                 "</INCTRAN>",
                 STMTRQCLOSE,
                 STMTTRNRQCLOSE,
-                BANKMSGSRQV1CLOSE,
-                "</OFX>" };
+                BANKMSGSRQV1CLOSE
+            };
 
             string result = string.Join("", body);
             return result;
+        }
+
+        string GetValidStatementRequestBodyString(OFXRequestConfigAccountType accountType)
+        {
+            string signon = GetValidSignOnRequestString();
+            string request = GetValidBankRequestString(accountType, true);
+            return "<OFX>" + signon + request + "</OFX>";
+        }
+
+        string GetValidBalanceRequestBodyString(OFXRequestConfigAccountType accountType)
+        {
+            string signon = GetValidSignOnRequestString();
+            string request = GetValidBankRequestString(accountType, false);
+            return "<OFX>" + signon + request + "</OFX>";
+        }
+
+        string GetValidAccountListRequestBodyString()
+        {
+            string signon = GetValidSignOnRequestString();
+            string request = GetValidAccountListRequestString();
+            return "<OFX>" + signon + request + "</OFX>";
+        }
+
+        string GetValidSignOnRequestBodyString()
+        {
+            string signon = GetValidSignOnRequestString();
+            return "<OFX>" + signon + "</OFX>";
         }
     }
 }
