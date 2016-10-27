735cb48e31abf574b4c87303f64b227e6c76b0e0
White Rabbit <White Rabbit@YOGA>
2016-10-26 20:07:09 -0700

    Add file to project

---
+++ Budget.DAL/Models/BalanceModel.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Budget.DAL.Models
+{
+    public class BalanceModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Key]
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Key]
+        [Required]
+        [Display(Name = "Balance As Of Date")]
+        public DateTime AsOfDate { get; set; }
+
+        [ForeignKey("AccountId")]
+        [JsonIgnore]
+        public AccountModel Account { get; set; }
+
+    }
+}
---
+++ Budget.DAL/Models/FinancialInstitutionModel.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Budget.DAL.Models
+{
+    public class FinancialInstitutionModel
+    {
+        [Required]
+        [Display(Name = "Financial Institution ID")]
+        public int Id { get; set; }
+
+        [Key]
+        [Required]
+        [Display(Name = "OFX ID")]
+        public int OfxId { get; set; }
+
+        [Required]
+        [Display(Name = "Financial Institution Name")]
+        public string Name { get; set; }
+
+        [Key]
+        [Required]
+        [Display(Name = "OFX URL")]
+        [Url]
+        public string OfxUrl { get; set; }
+
+        [Required]
+        [Display(Name = "OFX Org Attribute")]
+        public string Org { get; set; }
+    }
+}
