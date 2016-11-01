85f45f246eb95237d9ff48ff9aec4cbb42fbb7f2
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-11-01 15:08:48 -0700

    File rename

--- Budget.DAL/Models/SubCategoryModel.cs
+++ Budget.DAL/Models/SubCategoryModel.cs
@@ -1,15 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Budget.DAL.Models
 {
-    public class CategoryModel
+    public class SubCategoryModel
     {
         [Key]
+        [Column(Order = 1)]
         [Required]
         public int Id { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [Required]
+        public int CategoryId { get; set; }
 
         [Required]
         public string Name { get; set; }
 
+        [ForeignKey("CategoryId")]
+        [JsonIgnore]
+        public virtual CategoryModel Category { get; set; }
     }
 }
