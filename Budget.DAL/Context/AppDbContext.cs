ef21087ec4efc4a5347cdd30fe1bbddd48a5191f
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-11-01 17:02:41 -0700

    Add file to project

---
+++ Budget.DAL/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Budget.DAL.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Budget.DAL
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IAppDbContext
+    {
+        public DbSet<AccountModel> Accounts { get; set; }
+        //DbSet<BalanceModel> Balances { get; set; }
+        public DbSet<CategoryModel> Categories { get; set; }
+        public DbSet<FinancialInstitutionModel> FinancialInstitutions { get; set; }
+        public DbSet<PayeeModel> Payees { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Budget.DAL/Context/IAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Budget.DAL.Models;
+using System.Data.Entity;
+
+namespace Budget.DAL
+{
+    internal interface IAppDbContext
+    {
+        DbSet<AccountModel> Accounts { get; set; }
+        //DbSet<BalanceModel> Balances { get; set; }
+        DbSet<CategoryModel> Categories { get; set; }
+        DbSet<FinancialInstitutionModel> FinancialInstitutions { get; set; }
+        DbSet<PayeeModel> Payees { get; set; }
+    }
+}
