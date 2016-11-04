8332bd389156cfe0852ee2ad8080140a77bff37d
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-11-04 12:05:28 -0700

    Removed external login methods

--- Budget.API/Models/UserAccountBindingModels.cs
+++ Budget.API/Models/UserAccountBindingModels.cs
@@ -6,13 +6,6 @@ namespace Budget.DAL.Models
 {
     // Models used as parameters to AccountController actions.
 
-    public class AddExternalLoginBindingModel
-    {
-        [Required]
-        [Display(Name = "External access token")]
-        public string ExternalAccessToken { get; set; }
-    }
-
     public class ChangePasswordBindingModel
     {
         [Required]
@@ -52,24 +45,6 @@ namespace Budget.DAL.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-    }
-
-    public class RegisterExternalBindingModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
-
-    public class RemoveLoginBindingModel
-    {
-        [Required]
-        [Display(Name = "Login provider")]
-        public string LoginProvider { get; set; }
-
-        [Required]
-        [Display(Name = "Provider key")]
-        public string ProviderKey { get; set; }
     }
 
     public class SetPasswordBindingModel
---
+++ Budget.API/Models/UserAccountViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Budget.DAL.Models
+{
+    // Models returned by AccountController actions.
+
+    public class UserInfoViewModel
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
