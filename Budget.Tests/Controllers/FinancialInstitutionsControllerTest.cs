eb5069c650c541378eab9ad732a1d8261b63f4d3
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-11-09 14:19:21 -0800

    Add file to project

---
+++ Budget.Tests/Controllers/FinancialInstitutionsControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Budget.Tests.Controllers
+{
+    [TestClass]
+    public class FinancialInstitutionsControllerTest
+    {
+        [TestMethod]
+        public void FICreate()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(false);
+        }
+    }
+}
