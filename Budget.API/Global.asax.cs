4edb4b0f9ad4f5e5d20520b82050d2984c0b2f9f
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-11-09 14:19:41 -0800

    Added unity

--- Budget.API/Global.asax.cs
+++ Budget.API/Global.asax.cs
@@ -7,6 +7,7 @@ namespace Budget.API
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            UnityConfig.RegisterComponents();
         }
     }
 }
