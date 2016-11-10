c538cb2718a72d87594f06a2065004982cf3e4e4
White Rabbit <White Rabbit@YOGA>
2016-11-09 22:38:34 -0800

    Update unity config

--- Budget.API/App_Start/UnityConfig.cs
+++ Budget.API/App_Start/UnityConfig.cs
@@ -1,9 +1,7 @@
 using Budget.DAL;
 using Microsoft.Practices.Unity;
-using Microsoft.Practices.Unity.WebApi;
 using System.Web.Http;
 using Unity.WebApi;
-
 
 namespace Budget.API
 {
@@ -12,14 +10,14 @@ namespace Budget.API
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
 
             // e.g. container.RegisterType<ITestService, TestService>();
-            container.RegisterType<ApplicationDbContext>();
-
-            GlobalConfiguration.Configuration.DependencyResolver = new UnityHierarchicalDependencyResolver(container);
+            container.RegisterType<IApplicationDbContext, ApplicationDbContext>(new HierarchicalLifetimeManager());
+            
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
 }
