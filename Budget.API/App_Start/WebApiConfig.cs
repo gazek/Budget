8174bdfe48146719ae0eda20d8b1526f479133a9
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-11-10 14:05:32 -0800

    Removed MapHttpRoutes

--- Budget.API/App_Start/WebApiConfig.cs
+++ Budget.API/App_Start/WebApiConfig.cs
@@ -14,12 +14,6 @@ namespace Budget.API
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
         }
     }
 }
