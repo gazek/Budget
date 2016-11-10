51912a2d70881b4923d03073bc09ca6826dd5cee
White Rabbit <White Rabbit@YOGA>
2016-11-09 19:14:30 -0800

    changed field to use IApplicationDbContext for testing

--- Budget.API/Controllers/FinancialInstitutionsController.cs
+++ Budget.API/Controllers/FinancialInstitutionsController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Web.Http;
 using Budget.API.Models;
 using Budget.DAL;
 using Microsoft.AspNet.Identity.Owin;
-using Budget.DAL.Models;
 using Budget.API.Services;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 
 namespace Budget.API.Controllers
@@ -18,31 +13,14 @@ namespace Budget.API.Controllers
     [RoutePrefix("api/FinancialInstitution")]
     public class FinancialInstitutionsController : ApiController
     {
-        private ApplicationDbContext _dbContext;
+        private IApplicationDbContext _dbContext;
         private ApplicationUserManager _userManager;
 
-        public FinancialInstitutionsController()
-        {
-            _dbContext = new ApplicationDbContext();
-        }
-
-        public FinancialInstitutionsController(ApplicationDbContext dbContext)
+        public FinancialInstitutionsController(IApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
-
-        public FinancialInstitutionsController(ApplicationUserManager userManager)
-        {
-            _dbContext = new ApplicationDbContext();
-            UserManager = userManager;
-        }
-
-        public FinancialInstitutionsController(ApplicationDbContext dbContext, ApplicationUserManager userManager)
-        {
-            _dbContext = dbContext;
-            UserManager = userManager;
-        }
-
+       
         public ApplicationUserManager UserManager
         {
             get
@@ -65,8 +43,9 @@ namespace Budget.API.Controllers
                 return BadRequest(ModelState);
             }
 
-            var ent = ModelMapper.BindingToEntity(model, User);
-            var record = _dbContext.FinancialInstitutions.Add(ent);
+            var entity = ModelMapper.BindingToEntity(model, User);
+
+            var record = _dbContext.FinancialInstitutions.Add(entity);
             try
             {
                 var result = _dbContext.SaveChanges();
@@ -101,5 +80,7 @@ namespace Budget.API.Controllers
 
             return BadRequest();
         }
+
+
     }
 }
