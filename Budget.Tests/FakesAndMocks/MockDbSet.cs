0a0967905c8ffbbcb3b939a17d113967e82b813e
White Rabbit <White Rabbit@YOGA>
2016-11-11 20:21:42 -0800

    Add file to project

---
+++ Budget.Tests/FakesAndMocks/MockDbSet.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Budget.API.Tests.FakesAndMocks
+{
+    class MockDbSet<T> where T : class
+    {
+
+        private IQueryable<T> DataSet;
+
+        public MockDbSet<T> UsingDataSet(IQueryable<T> dataSet)
+        {
+            DataSet = dataSet;
+            return this;
+        }
+
+        public Mock<DbSet<T>> Mock()
+        {
+            Mock<DbSet<T>> theMock = new Mock<DbSet<T>>();
+            theMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(DataSet.Provider);
+            theMock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(DataSet.Expression);
+            theMock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(DataSet.ElementType);
+            theMock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(DataSet.GetEnumerator());
+            return theMock;
+        }
+    }
+}
---
+++ Budget.Tests/FakesAndMocks/UserBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Budget.API.Tests.Fakes
+{
+    public static class UserBuilder
+    {
+        public static IPrincipal CreateUser()
+        {
+            // create user mock
+            var userMock = new Moq.Mock<IPrincipal>();
+
+            // Create a fake Identity
+            // Cannot use Moq since GetUserId() is an extension method
+            string userId = Guid.NewGuid().ToString();
+            List<Claim> claims = new List<Claim>
+                {
+                    new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userId)
+                };
+            var identityMock = new ClaimsIdentity(claims);
+            userMock.SetupGet(x => x.Identity).Returns(identityMock);
+
+            // assign to field
+            return userMock.Object;
+        }
+    }
+}
