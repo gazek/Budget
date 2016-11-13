6e5cbde47a3a0f6a9675d4794f64852c4bcb0d0d
White Rabbit <White Rabbit@YOGA>
2016-11-12 21:46:51 -0800

    Add file to project

---
+++ Budget.API/Models/AccountViewModels.cs
@@ -0,0 +1,12 @@
+﻿using Budget.DAL.Models;
+
+namespace Budget.API.Models
+{
+    public class AccountListViewModel
+    {
+        public int FinancialInstitutionId { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public AccountType Type { get; set; }
+    }
+}
