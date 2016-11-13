1e2389808189bcf20f0c0ed2a1faeaecc4d5c918
White Rabbit <White Rabbit@YOGA>
2016-11-12 21:47:49 -0800

    Add file to project

---
+++ Budget.API/Services/OFXClient/OfxClient.cs
@@ -0,0 +1,37 @@
+﻿namespace Budget.API.Services.OFXClient
+{
+    public class OfxClient
+    {
+        public OFXRequestConfig RequestConfig { get; set; }
+        public OFXRequestBuilder RequestBuilder { get; set; }
+        public OFXRequestor Requestor { get; set; }
+        public OFXParser Parser { get; set; }
+
+        public OfxClient()
+        {
+            RequestConfig = new OFXRequestConfig();
+        }
+
+        public OfxClient(OFXRequestConfig config)
+        {
+            RequestConfig = config;
+        }
+
+        public void BuildRequest()
+        {
+            RequestBuilder = new OFXRequestBuilder(RequestConfig);
+        }
+
+        public void ExecuteRequest()
+        {
+            Requestor = new OFXRequestor(RequestBuilder);
+            Requestor.Post();
+        }
+
+        public void ParseResponse()
+        {
+            Parser = new OFXParser(Requestor.OFX);
+            Parser.Parse();
+        }
+    }
+}
