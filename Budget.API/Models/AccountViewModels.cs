4417daf7ac13542c5652175ca5e7ef93590db8de
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-11-14 16:46:48 -0800

    Add view model

--- Budget.API/Models/AccountViewModels.cs
+++ Budget.API/Models/AccountViewModels.cs
@@ -1,7 +1,20 @@
 ﻿using Budget.DAL.Models;
+using System.Collections.Generic;
 
 namespace Budget.API.Models
 {
+    public class AccountViewModel
+    {
+        public int Id { get; set; }
+        public int FinancialInstitutionId { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public AccountType Type { get; set; }
+        public string Description { get; set; }
+        public ICollection<TransactionModel> Transactions { get; set; }
+        public BalanceModel Balance { get; set; }
+    }
+
     public class AccountListViewModel
     {
         public int FinancialInstitutionId { get; set; }
