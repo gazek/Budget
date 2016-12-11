4bf6f7dc9d8994dec86e7717d6112d0faeccc519
White Rabbit <White Rabbit@YOGA>
2016-12-11 09:49:20 -0800

    Add file to project

---
+++ Budget.Tests/Services/TransactionDefaultsTests.cs
@@ -0,0 +1,358 @@
+﻿using Budget.API.Services;
+using Budget.API.Tests.Fakes;
+using Budget.API.Tests.FakesAndMocks;
+using Budget.DAL;
+using Budget.DAL.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+
+namespace Budget.API.Tests.Services
+{
+    [TestClass]
+    public class TransactionDefaultsTests
+    {
+        [TestMethod]
+        public void GetDefaultStatus()
+        {
+            // Arrange
+            var defaults = new TransactionDefaults(new MockDbContext().GetMock());
+
+            // Act
+            var result = defaults.GetDefaultStatus();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(TransactionStatus));
+            Assert.AreEqual(TransactionStatus.New, result);
+        }
+
+        [TestMethod]
+        public void GetDefaultDateAdded()
+        {
+            // Arrange
+            var defaults = new TransactionDefaults(new MockDbContext().GetMock());
+
+            // Act
+            var result = defaults.GetDefaultDateAdded();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(DateTime));
+            Assert.AreEqual(DateTime.Now.Date, result.Date);
+        }
+
+        [TestMethod]
+        public void GetDefaultLastEditDate()
+        {
+            // Arrange
+            var defaults = new TransactionDefaults(new MockDbContext().GetMock());
+
+            // Act
+            var result = defaults.GetDefaultLastEditDate();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(DateTime));
+            Assert.AreEqual(DateTime.Now.Date, result.Date);
+        }
+
+        [TestMethod]
+        public void GetDefaultPayeesSingleMatch()
+        {
+            // Arrange
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var inputPayeeName = "FooBar";
+            var defaults = new TransactionDefaults(contextMock.Object);
+
+            // Act
+            var result = defaults.GetDefaultPayees(user.Identity.GetUserId(), inputPayeeName);
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void GetDefaultPayeesSingleMatchCaseInsensitive()
+        {
+            // Arrange
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var inputPayeeName = "OTHER PAYEE";
+            var defaults = new TransactionDefaults(contextMock.Object);
+
+            // Act
+            var result = defaults.GetDefaultPayees(user.Identity.GetUserId(), inputPayeeName);
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void GetDefaultPayeesMultipleMatch()
+        {
+            // Arrange
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var inputPayeeName = "FooBar2";
+            var defaults = new TransactionDefaults(contextMock.Object);
+
+            // Act
+            var result = defaults.GetDefaultPayees(user.Identity.GetUserId(), inputPayeeName);
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void GetDefaultPayeesNoMatch()
+        {
+            // Arrange
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var inputPayeeName = "nothing should match this";
+            var defaults = new TransactionDefaults(contextMock.Object);
+
+            // Act
+            var result = defaults.GetDefaultPayees(user.Identity.GetUserId(), inputPayeeName);
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void GetDefaultPayeeDetailsWithOnePayeeNoTransaction()
+        {
+            // Arrange
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var defaults = new TransactionDefaults(contextMock.Object);
+            var payee = contextMock.Object.Payees.Where(x => x.Name.Equals("otherone")).First();
+
+            // Act
+            var result = defaults.GetDefaultPayeeDetails(payee);
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(22, result[0].CategoryId);
+            Assert.AreEqual(20, result[0].SubCategoryId);
+        }
+
+        [TestMethod]
+        public void GetDefaultPayeeDetailsWithManyPayeesNoTransaction()
+        {
+            // Arrange
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var defaults = new TransactionDefaults(contextMock.Object);
+            var payee = contextMock.Object.Payees.Where(x => x.Name.Equals("FooBar2")).First();
+
+            // Act
+            var result = defaults.GetDefaultPayeeDetails(payee);
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void GetTopPayeeNameNone()
+        {
+            // Arrange
+            var payeeName = "no details";
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var defaults = new TransactionDefaults(contextMock.Object);
+            var payee = contextMock.Object.Payees.Where(x => x.Name.Equals(payeeName)).First();
+            var details = defaults.GetDefaultPayeeDetails(payee);
+            var transaction = new TransactionModel()
+            {
+                Details = details
+            };
+            // Act
+            var result = defaults.GetTopPayeeName(transaction);
+
+            // Assert
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void GetTopPayeeNameSinglePayee()
+        {
+            // Arrange
+            var payeeName = "otherone";
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var defaults = new TransactionDefaults(contextMock.Object);
+            var payee = contextMock.Object.Payees.Where(x => x.Name.Equals(payeeName)).First();
+            var details = defaults.GetDefaultPayeeDetails(payee);
+            var transaction = new TransactionModel()
+            {
+                Details = details
+            };
+            // Act
+            var result = defaults.GetTopPayeeName(transaction);
+
+            // Assert
+            Assert.AreEqual(payeeName, result);
+        }
+
+        [TestMethod]
+        public void GetTopPayeeNameMultiplePayees()
+        {
+            // Arrange
+            var user = UserBuilder.CreateUser();
+            var contextMock = MakeContext(user);
+            var defaults = new TransactionDefaults(contextMock.Object);
+            var payees = contextMock.Object.Payees
+                .Where(x => x.Name.Contains("Foo"))
+                .Where(x => x.UserId.Equals(user.Identity.GetUserId()))
+                .ToList();
+            var details = defaults.GetDefaultPayeeDetails(payees[0]);
+            details.AddRange(defaults.GetDefaultPayeeDetails(payees[1]));
+            var transaction = new TransactionModel()
+            {
+                Details = details
+            };
+            // Act
+            var result = defaults.GetTopPayeeName(transaction);
+
+            // Assert
+            Assert.AreEqual("Multiple", result);
+        }
+
+        private Mock<IApplicationDbContext> MakeContext(IPrincipal user)
+        {
+            var detail1 = new PayeeDefaultDetailsModel()
+            {
+                PayeeId = 1,
+                CategoryId = 11,
+                SubCategoryId = 10,
+                Allocation = 100
+            };
+            var detail2 = new PayeeDefaultDetailsModel()
+            {
+                PayeeId = 2,
+                CategoryId = 22,
+                SubCategoryId = 20,
+                Allocation = 100
+            };
+            var detail41 = new PayeeDefaultDetailsModel()
+            {
+                PayeeId = 4,
+                CategoryId = 44,
+                SubCategoryId = 40,
+                Allocation = 75
+            };
+            var detail42 = new PayeeDefaultDetailsModel()
+            {
+                PayeeId = 4,
+                CategoryId = 44,
+                SubCategoryId = 41,
+                Allocation = 25,
+            };
+            var payee1 = new PayeeModel()
+            {
+                Id = 1,
+                UserId = user.Identity.GetUserId(),
+                Name = "Foo Bar",
+                ImportNames = new List<ImportNameToPayeeModel>()
+                {
+                    new ImportNameToPayeeModel()
+                    {
+                        PayeeId = 1,
+                        ImportName = "FooBar"
+                    },
+                    new ImportNameToPayeeModel()
+                    {
+                        PayeeId = 1,
+                        ImportName = "Foo Bar"
+                    }
+                },
+                DefaultDetails = new List<PayeeDefaultDetailsModel>() { detail1 }
+            };
+            var payee2 = new PayeeModel()
+            {
+                Id = 2,
+                UserId = user.Identity.GetUserId(),
+                Name = "otherone",
+                ImportNames = new List<ImportNameToPayeeModel>()
+                {
+                    new ImportNameToPayeeModel()
+                    {
+                        PayeeId = 1,
+                        ImportName = "other payee"
+                    },
+                    new ImportNameToPayeeModel()
+                    {
+                        PayeeId = 1,
+                        ImportName = "other1"
+                    }
+                },
+                DefaultDetails = new List<PayeeDefaultDetailsModel>() { detail2 }
+            };
+            var payee3 = new PayeeModel()
+            {
+                Id = 3,
+                UserId = Guid.NewGuid().ToString(),
+                Name = "Foo Bar",
+                ImportNames = new List<ImportNameToPayeeModel>()
+                {
+                    new ImportNameToPayeeModel()
+                    {
+                        PayeeId = 1,
+                        ImportName = "FooBar"
+                    },
+                    new ImportNameToPayeeModel()
+                    {
+                        PayeeId = 1,
+                        ImportName = "Foo Bar"
+                    }
+                }
+            };
+            var payee4 = new PayeeModel()
+            {
+                Id = 3,
+                UserId = user.Identity.GetUserId(),
+                Name = "FooBar2",
+                ImportNames = new List<ImportNameToPayeeModel>()
+                {
+                    new ImportNameToPayeeModel()
+                    {
+                        PayeeId = 1,
+                        ImportName = "Foobar2"
+                    }
+                },
+                DefaultDetails = new List<PayeeDefaultDetailsModel>() { detail41, detail42 }
+            };
+            var payee5 = new PayeeModel()
+            {
+                Id = 3,
+                UserId = user.Identity.GetUserId(),
+                Name = "no details",
+                ImportNames = new List<ImportNameToPayeeModel>()
+                {
+                    new ImportNameToPayeeModel()
+                    {
+                        PayeeId = 1,
+                        ImportName = "no details"
+                    }
+                },
+                DefaultDetails = new List<PayeeDefaultDetailsModel>()
+            };
+            var payees = new List<PayeeModel>() { payee1, payee2, payee3, payee4, payee5};
+            // mock context
+            var contextMockBuilder = new MockDbContext()
+                .WithData(payees)
+                .SetupFind(1, payee1)
+                .SetupFind(2, payee2)
+                .SetupFind(3, payee3)
+                .SetupFind(4, payee4)
+                .SetupFind(5, payee5)
+                .Finalize();
+
+            return contextMockBuilder.Context;
+        }
+    }
+}
---
+++ Budget.Tests/Services/TransactionImporterTest.cs
@@ -0,0 +1,245 @@
+﻿using Budget.API.Services;
+using Budget.API.Tests.Fakes;
+using Budget.API.Tests.FakesAndMocks;
+using Budget.DAL;
+using Budget.DAL.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+
+namespace Budget.API.Tests.Services
+{
+    [TestClass]
+    public class TransactionImporterTest
+    {
+        [TestMethod]
+        public void TransactionImporterConstructorNoneExistingTest()
+        {
+            // Arrange
+            var context = MakeContext().Object;
+            var defaults = new TransactionDefaults(context);
+            var trans = GetFakeTransactions();
+            var account = GetFakeAccount(new List<TransactionModel>() { trans[0] });
+            trans.RemoveAt(0);
+
+            // Act
+            var importer = new TransactionImporter(trans, account, context);
+
+            // Assert
+            Assert.AreEqual(0, importer.Transactions.Select(x => x.Id).Aggregate((acc, x) => acc + x));
+        }
+
+        [TestMethod]
+        public void TransactionImporterConstructorWithExistingTest()
+        {
+            // Arrange
+            var context = MakeContext().Object;
+            var defaults = new TransactionDefaults(context);
+            var trans = GetFakeTransactions();
+            var account = GetFakeAccount(new List<TransactionModel>() { trans[0] });
+            var sum = context.Transactions.First().Id;
+
+            // Act
+            var importer = new TransactionImporter(trans, account, context);
+
+            // Assert
+            Assert.AreEqual(sum, importer.Transactions.Select(x => x.Id).Aggregate((acc, x) => acc + x));
+        }
+
+        [TestMethod]
+        public void TransactionImporterFilterExistingTest()
+        {
+            // Arrange
+            var context = MakeContext().Object;
+            var defaults = new TransactionDefaults(context);
+            var trans = GetFakeTransactions();
+            var account = GetFakeAccount(new List<TransactionModel>() { trans[0] });
+            var sum = context.Transactions.First().Id;
+            var importer = new TransactionImporter(trans, account, context);
+
+            // Act
+            importer.FilterExisting();
+
+            // Assert
+            Assert.AreEqual(3, trans.Count);
+            Assert.AreEqual(2, importer.Transactions.Count);
+        }
+
+        [TestMethod]
+        public void TransactionImporterApplyDefaultsDefaultValueTest()
+        {
+            // Arrange
+            var context = MakeContext().Object;
+            var defaults = new Mock<TransactionDefaults>();
+            defaults.Setup(x => x.GetDefaultPayees(It.IsAny<string>(), It.IsAny<string>())).Returns(new List<PayeeModel>());
+            var trans = GetFakeTransactions();
+            var account = GetFakeAccount(new List<TransactionModel>() { trans[0] });
+            var importer = new TransactionImporter(trans, account, context);
+
+            // Act
+            importer.SetDefaultStatus().ApplyDefaults();
+
+
+            // Assert
+            foreach (TransactionModel t in importer.Transactions)
+            {
+                Assert.AreEqual(TransactionStatus.New, t.Status);
+            }
+        }
+
+        [TestMethod]
+        public void TransactionImporterApplyDefaultsCustomValueTest()
+        {
+            // Arrange
+            var context = MakeContext().Object;
+            var defaults = new Mock<TransactionDefaults>();
+            defaults.Setup(x => x.GetDefaultPayees(It.IsAny<string>(), It.IsAny<string>())).Returns(new List<PayeeModel>());
+            var trans = GetFakeTransactions();
+            var account = GetFakeAccount(new List<TransactionModel>() { trans[0] });
+            var importer = new TransactionImporter(trans, account, context);
+
+            // Act
+            importer.SetDefaultStatus(TransactionStatus.Void).ApplyDefaults();
+
+
+            // Assert
+            foreach (TransactionModel t in importer.Transactions)
+            {
+                Assert.AreEqual(TransactionStatus.Void, t.Status);
+            }
+        }
+
+        [TestMethod]
+        public void TransactionImporterApplyDefaultsDoNotModifyTest()
+        {
+            // Arrange
+            var context = MakeContext().Object;
+            var defaults = new Mock<TransactionDefaults>();
+            defaults.Setup(x => x.GetDefaultPayees(It.IsAny<string>(), It.IsAny<string>())).Returns(new List<PayeeModel>());
+            var trans = GetFakeTransactions();
+            trans[0].Status = TransactionStatus.Rejected;
+            var account = GetFakeAccount(new List<TransactionModel>() { trans[0] });
+            var importer = new TransactionImporter(trans, account, context);
+
+            // Act
+            importer.UnsetDefaultStatus().ApplyDefaults();
+
+            // Assert
+            for (int i = 0; i < trans.Count; ++i)
+            {
+                Assert.AreEqual(trans[i].Status, importer.Transactions[i].Status);
+            }
+        }
+
+        [TestMethod]
+        public void NEED_TransactionImporterApplyDefaultsDetailsSingleDetailTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void NEED_TransactionImporterApplyDefaultsDetailsMultipleDetailTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void NEED_TransactionImporterCommitTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void NEED_TransactionImporterCommitTestAddBalance()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(false);
+        }
+
+        private List<TransactionModel> GetFakeTransactions()
+        {
+            var trans1 = new TransactionModel()
+            {
+                ReferenceValue = "1",
+                Date = DateTime.Today.AddDays(-5),
+                OriginalPayeeName = "",
+            };
+
+            var trans2 = new TransactionModel()
+            {
+                ReferenceValue = "2",
+                OriginalPayeeName = "",
+                Date = DateTime.Today.AddDays(-5)
+            };
+
+            var trans3 = new TransactionModel()
+            {
+                ReferenceValue = "3",
+                OriginalPayeeName = "",
+                Date = DateTime.Today.AddDays(-4)
+            };
+
+            return new List<TransactionModel>() { trans1, trans2, trans3 };
+        }
+
+        private AccountModel GetFakeAccount(List<TransactionModel> trans)
+        {
+            var user = new ApplicationUser()
+            {
+                Id = "123"
+            };
+            return new AccountModel()
+            {
+                Id = 13,
+                Transactions = trans,
+                UserId = user.Id,
+                User = user
+            };
+        }
+
+        private Mock<IApplicationDbContext> MakeContext()
+        {
+            // get transactions
+            var trans1 = GetFakeTransactions()[0];
+            trans1.Id = 1;
+            var trans = new List<TransactionModel>() { trans1 };
+
+            // get account
+            var account = GetFakeAccount(trans);
+
+            // mock context
+            var contextMockBuilder = new MockDbContext()
+                .WithData(trans)
+                .SetupFind(1, trans1)
+                .WithData(new List<AccountModel>() { account })
+                .SetupFind(13, account)
+                .WithData(new List<PayeeModel>())
+                .Finalize();
+
+            return contextMockBuilder.Context;
+        }
+    }
+}
