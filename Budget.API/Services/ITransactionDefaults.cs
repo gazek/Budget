dea9ca44c1f90160e05392ca1e227b6d354f787a
White Rabbit <White Rabbit@YOGA>
2016-12-14 20:08:00 -0800

    Add file to project

---
+++ Budget.API/Services/ITransactionDefaults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Budget.DAL.Models;
+
+namespace Budget.API.Services
+{
+    public interface ITransactionDefaults
+    {
+        DateTime GetDefaultDateAdded();
+        DateTime GetDefaultLastEditDate();
+        List<TransactionDetailModel> GetDefaultPayeeDetails(PayeeModel payee, TransactionModel transaction = null);
+        List<PayeeModel> GetDefaultPayees(string userId, string originalPayeeName);
+        TransactionStatus GetDefaultStatus();
+        string GetTopPayeeName(TransactionModel transaction);
+    }
+}
