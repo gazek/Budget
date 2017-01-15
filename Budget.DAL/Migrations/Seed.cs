a8a0554ed0a9b9eaf5ff18349c1052b77375f9fe
White Rabbit <White Rabbit@YOGA>
2017-01-15 14:30:40 -0800

    Add file to project

---
+++ Budget.DAL/Migrations/Seed.cs
@@ -0,0 +1,12 @@
+﻿namespace Budget.DAL.Migrations
+{
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+
+    internal sealed partial class Configuration : DbMigrationsConfiguration<Budget.DAL.ApplicationDbContext>
+    {
+
+    }
+}
