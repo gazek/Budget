88dbfaf24c51f6d5ab83d85f89861103b35279e1
White Rabbit <White Rabbit@YOGA>
2017-01-22 12:25:45 -0800

    Add file to project

--- Budget.API/Controllers/AccountController.cs
+++ Budget.API/Controllers/AccountController.cs
@@ -1,10 +1,10 @@
 ﻿using Budget.API.Models;
-using Budget.API.Services;
 using Budget.DAL;
 using Budget.DAL.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Http;
 
@@ -24,6 +24,7 @@ namespace Budget.API.Controllers
         [Authorize]
         public IHttpActionResult Create(int fiId, AccountBindingModel model)
         {
+            model.FinancialInstitutionId = fiId;
             Func<int, string> location = x => Url.Link("GetAccountById", new { id = x });
             return base.Create<AccountBindingModel, FinancialInstitutionModel>(model, fiId);
         }
@@ -38,29 +39,72 @@ namespace Budget.API.Controllers
         }
 
         // get all accounts owned by user
-        [Route("Account/", Name = "GetAllAccounts")]
+        [Route("Account", Name = "GetAllAccountsByUserId")]
         [HttpGet]
         [Authorize]
-        public override IHttpActionResult GetAll()
+        public IHttpActionResult GetAllByUser()
         {
-            return base.GetAll<AccountModel>();
+            // find all Financial Institutions owned by user
+            string userId = User.Identity.GetUserId();
+            var fiIds = _dbContext.FinancialInstitutions
+                .Where(f => f.UserId == userId)
+                .Select(f => f.Id);
+
+            // filters
+            List<Expression<Func<AccountModel, bool>>> filters = new List<Expression<Func<AccountModel, bool>>>();
+            filters.Add(a => fiIds.Contains(a.FinancialInstitutionId));
+
+            // get related entities
+            var include = new List<string>();
+            include.Add("Transactions");
+            include.Add("BalanceHistory");
+
+            return GetAll<AccountModel, string>(filters, include, a => a.Name);
+        }
+
+        // get all accounts by Fi ID
+        [Route("FinancialInstitution/{fiId}/Account/", Name = "GetAllAccountsByFI")]
+        [HttpGet]
+        [Authorize]
+        public IHttpActionResult GetAllByFI(int fiId)
+        {
+            // filters
+            List<Expression<Func<AccountModel, bool>>> filters = new List<Expression<Func<AccountModel, bool>>>();
+            filters.Add(a => a.FinancialInstitutionId == fiId);
+
+            // get related entities
+            var include = new List<string>();
+            //include.Add("Transactions");
+
+            // verify existence of account
+            // and that user is authorized to access it
+            GetRecordAndIsAuthorized<FinancialInstitutionModel>(fiId);
+
+            if (!_requestIsOk)
+            {
+                return _errorResponse;
+            }
+
+            return base.GetAll<AccountModel, string>(filters, include, a => a.Name);
         }
 
         // update account properties when owned by user
-        [Route("Account/{id}", Name = "UpdateAccount")]
+        [Route("FinancialInstitution/{fiId}/Account/{id}", Name = "UpdateAccount")]
         [HttpPut]
         [Authorize]
-        public override IHttpActionResult Update<AccountBindingModel>(int id, AccountBindingModel model)
+        public IHttpActionResult Update(int id, int fiId, AccountBindingModel model)
         {
+            model.FinancialInstitutionId = fiId;
             return base.Update(id, model);
         }
 
-        // TODO:
-        //   Add routes to query by: amount
-        //                           payee
-        //                           category
-        //                           subcategory
-        //  Should this API be under account or transactions or both?
-        
+        // delete account
+        [Route("Account/{id}", Name = "DeleteAccount")]
+        [HttpDelete]
+        [Authorize]
+        public override IHttpActionResult Delete(int id)
+        {
+            return base.Delete(id);
+        }
     }
 }
---
+++ Budget.API/Controllers/SubCategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Budget.API.Models;
+using Budget.API.Services;
+using Budget.DAL;
+using Budget.DAL.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Principal;
+using System.Web.Http;
+
+namespace Budget.API.Controllers
+{
+    [Authorize]
+    public class SubCategoryController : ControllerBase<SubCategoryModel>
+    {
+        public SubCategoryController(IApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        [Route("api/Category/{catId}/SubCategory/{id}", Name = "DeleteSubCategory")]
+        [HttpDelete]
+        [Authorize]
+        public override IHttpActionResult Delete(int id)
+        {
+            return Delete(id);
+        }
+
+        [Route("api/Category/{catId}/SubCategory", Name = "CreateSubCategory")]
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult Create(SubCategoryBindingModel model, int catId)
+        {
+            // Add CategoryId to model
+            model.CategoryId = catId;
+            // call base create
+            return Create<SubCategoryBindingModel, CategoryModel>(model, catId);
+        }
+
+        [Route("api/Category/{catId}/SubCategory/{id}", Name = "UpdateSubCategory")]
+        [HttpPut]
+        public IHttpActionResult Update(SubCategoryBindingModel model, int id, int catId)
+        {
+            // Add CategoryId to model
+            model.CategoryId = catId;
+            return Update(id, model);
+        }
+
+        [Route("api/Category/{catId}/SubCategory/{id}", Name = "GetSubCategoryById")]
+        [HttpGet]
+        [Authorize]
+        public override IHttpActionResult Get(int id)
+        {
+            return base.Get(id);
+        }
+    }
+}
